22e88623f492b99d0632b33d88677a1cb6fcf05e
mdujic <mdujic212@gmail.com>
2023-01-17 22:28:53 +0100

    finished decorating loading screen

--- city_building/LoadingScreen.Designer.cs
+++ city_building/LoadingScreen.Designer.cs
@@ -29,11 +29,92 @@
 		private void InitializeComponent()
 		{
 			this.components = new System.ComponentModel.Container();
+			this.BulbPicture = new System.Windows.Forms.PictureBox();
+			this.HintLbl = new System.Windows.Forms.Label();
+			this.HintRichTextBox = new System.Windows.Forms.RichTextBox();
+			this.LoadingLbl = new System.Windows.Forms.Label();
+			this.LoadingTimer = new System.Windows.Forms.Timer(this.components);
+			((System.ComponentModel.ISupportInitialize)(this.BulbPicture)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// BulbPicture
+			// 
+			this.BulbPicture.BackColor = System.Drawing.Color.White;
+			this.BulbPicture.Image = global::city_building.Properties.Resources.bulb;
+			this.BulbPicture.Location = new System.Drawing.Point(47, 21);
+			this.BulbPicture.Name = "BulbPicture";
+			this.BulbPicture.Size = new System.Drawing.Size(129, 91);
+			this.BulbPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.BulbPicture.TabIndex = 0;
+			this.BulbPicture.TabStop = false;
+			// 
+			// HintLbl
+			// 
+			this.HintLbl.AutoSize = true;
+			this.HintLbl.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.HintLbl.ForeColor = System.Drawing.Color.Maroon;
+			this.HintLbl.Location = new System.Drawing.Point(182, 21);
+			this.HintLbl.Name = "HintLbl";
+			this.HintLbl.Size = new System.Drawing.Size(68, 26);
+			this.HintLbl.TabIndex = 1;
+			this.HintLbl.Text = "Hint:";
+			// 
+			// HintRichTextBox
+			// 
+			this.HintRichTextBox.BackColor = System.Drawing.Color.White;
+			this.HintRichTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.HintRichTextBox.Enabled = false;
+			this.HintRichTextBox.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.HintRichTextBox.ForeColor = System.Drawing.Color.Maroon;
+			this.HintRichTextBox.Location = new System.Drawing.Point(256, 24);
+			this.HintRichTextBox.Name = "HintRichTextBox";
+			this.HintRichTextBox.Size = new System.Drawing.Size(214, 141);
+			this.HintRichTextBox.TabIndex = 2;
+			this.HintRichTextBox.Text = "";
+			// 
+			// LoadingLbl
+			// 
+			this.LoadingLbl.AutoSize = true;
+			this.LoadingLbl.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.LoadingLbl.ForeColor = System.Drawing.Color.Maroon;
+			this.LoadingLbl.Location = new System.Drawing.Point(353, 168);
+			this.LoadingLbl.Name = "LoadingLbl";
+			this.LoadingLbl.Size = new System.Drawing.Size(117, 26);
+			this.LoadingLbl.TabIndex = 3;
+			this.LoadingLbl.Text = "Loading...";
+			// 
+			// LoadingTimer
+			// 
+			this.LoadingTimer.Tick += new System.EventHandler(this.LoadingTimer_Tick);
+			// 
+			// LoadingScreen
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.BackColor = System.Drawing.Color.White;
+			this.ClientSize = new System.Drawing.Size(482, 203);
+			this.ControlBox = false;
+			this.Controls.Add(this.LoadingLbl);
+			this.Controls.Add(this.HintRichTextBox);
+			this.Controls.Add(this.HintLbl);
+			this.Controls.Add(this.BulbPicture);
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "LoadingScreen";
+			this.ShowIcon = false;
 			this.Text = "LoadingScreen";
+			((System.ComponentModel.ISupportInitialize)(this.BulbPicture)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private PictureBox BulbPicture;
+		private Label HintLbl;
+		private RichTextBox HintRichTextBox;
+		private Label LoadingLbl;
+		private System.Windows.Forms.Timer LoadingTimer;
 	}
 }
--- city_building/LoadingScreen.cs
+++ city_building/LoadingScreen.cs
@@ -14,10 +14,10 @@ namespace city_building
 	{
 		//Delegate for cross thread call to close
 		private delegate void CloseDelegate();
-
+		int time = 0;
 		//The type of form to be displayed as the splash screen.
 		private static LoadingScreen loadingScreen;
-
+		
 		static public void ShowLoadingScreen()
 		{
 			// Make sure it is only launched once.    
@@ -50,6 +50,52 @@ namespace city_building
 		public LoadingScreen()
 		{
 			InitializeComponent();
+			
+			// choose random integer from 1 to 10
+			Random rnd = new Random();
+			int r = rnd.Next(1, 5);
+
+			// set hint based on random integer
+			switch(r)
+			{
+				case 1:
+					HintRichTextBox.Text = "You can buy resources from the market.";
+					break;
+				case 2:
+					HintRichTextBox.Text = "You can sell resources to the market.";
+					break;
+				case 3:
+					HintRichTextBox.Text = "You can build buildings in the city.";
+					break;
+				case 4:
+					HintRichTextBox.Text = "Rather start with mining, than sell iron for other resources.";
+					break;
+				case 5:
+					HintRichTextBox.Text = "Build a lot towers so you can get strong flow of gold.";
+					break;
+			}
+		
+		}
+
+		private void LoadingTimer_Tick(object sender, EventArgs e)
+		{
+			time++;
+
+			switch (time % 4)
+			{
+				case 0:
+					LoadingLbl.Text = "Loading.";
+					break;
+				case 1:
+					LoadingLbl.Text = "Loading..";
+					break;
+				case 2:
+					LoadingLbl.Text = "Loading...";
+					break;
+				case 3:
+					LoadingLbl.Text = "Loading....";
+					break;
+			}
 		}
 	}
 }
